86d2ff9dfc6a9e1bad7e1867d20e3215ceda4921
xdillisx <xdillisx@gmail.com>
2023-01-10 08:14:02 -0500

    initial commit

---
+++ .install.file.deps.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Install dependencies
+sudo apt update && sudo apt install bashtop zsh zsh-antigen lsd batcat
+                                                                                                                                                                                        
