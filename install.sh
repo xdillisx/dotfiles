f1aa30e1f31c8852c295b5813cfc555434ac9919
xdillisx <xdillisx@gmail.com>
2023-11-06 20:10:54 -0500

    fixed dep install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install dependencies
-sudo apt update && sudo apt install btop zsh zsh-antigen bat autojump command-not-found packagekit-command-not-found exa stow
+sudo apt update && sudo apt install btop zsh bat autojump command-not-found packagekit-command-not-found exa stow tmux
 sudo update-command-not-found
 sudo apt-file update
 sudo chsh -s $(which zsh) $USER
